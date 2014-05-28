41629e47f2d989eb10b7f13bff55ebeb555e808e
Kasey Le <kaseyle@stanford.edu>
2014-05-27 21:37:28 -0700

    Fix update avatar menu

--- PocketLove/PLAvatarUpdateViewController.m
+++ PocketLove/PLAvatarUpdateViewController.m
@@ -15,10 +15,11 @@
 
 @property (nonatomic, strong) NSMutableArray *avatarMoodImages;
 @property (nonatomic, strong) NSMutableArray *avatarOutfitImages;
-@property (nonatomic, strong) NSIndexPath *selectedIndexPath;
+@property (nonatomic, strong) NSIndexPath *moodSelectedIndexPath;
+@property (nonatomic, strong) NSIndexPath *outfitSelectedIndexPath;
 @property (weak, nonatomic) IBOutlet UISwitch *callAvailabilitySwitch;
 @property (weak, nonatomic) IBOutlet UICollectionView *moodCollectionView;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *outfitControl;
+@property (weak, nonatomic) IBOutlet UICollectionView *outfitCollectionView;
 
 @end
 
@@ -31,6 +32,13 @@
     moodCollectionView.allowsMultipleSelection = NO;
 }
 
+- (void)setOutfitCollectionView:(UICollectionView *)outfitCollectionView
+{
+    _outfitCollectionView = outfitCollectionView;
+    outfitCollectionView.allowsSelection = YES;
+    outfitCollectionView.allowsMultipleSelection = NO;
+}
+
 - (NSMutableArray *)avatarMoodImages
 {
     if (!_avatarMoodImages) {
@@ -40,6 +48,17 @@
         }
     }
     return _avatarMoodImages;
+}
+
+- (NSMutableArray *)avatarOutfitImages
+{
+    if (!_avatarOutfitImages) {
+        _avatarOutfitImages = [[NSMutableArray alloc] init];
+        for (NSString *outfit in [PLConstants outfitStrings]) {
+            [_avatarOutfitImages addObject:[UIImage imageNamed:[NSString stringWithFormat:@"%@_Outfit_%@", [self stringForGender:self.avatar.gender], outfit]]];
+        }
+    }
+    return _avatarOutfitImages;
 }
 
 - (NSString *)stringForGender:(NSUInteger)gender
@@ -56,14 +75,19 @@
     
     //synchronize with avatar state
     self.callAvailabilitySwitch.on = self.avatar.isAvailableForCall;
-    self.outfitControl.selectedSegmentIndex = self.avatar.outfit;
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:self.avatar.mood inSection:0];
-    [self highlightCellAtIndexPath:indexPath];
+    [self highlightCellAtIndexPath:indexPath collectionView:self.moodCollectionView];
     [self.moodCollectionView selectItemAtIndexPath:indexPath
+                                          animated:YES
+                                    scrollPosition:UICollectionViewScrollPositionLeft];
+    
+    NSIndexPath *indexPath2 = [NSIndexPath indexPathForRow:self.avatar.outfit inSection:0];
+    [self highlightCellAtIndexPath:indexPath2 collectionView:self.outfitCollectionView];
+    [self.outfitCollectionView selectItemAtIndexPath:indexPath2
                                           animated:YES
                                     scrollPosition:UICollectionViewScrollPositionLeft];
 }
@@ -74,8 +98,8 @@
 {
     if ([segue.identifier isEqualToString:@"DismissAvatarUpdate"]) {
         self.avatar.callAvailability = self.callAvailabilitySwitch.isOn;
-        self.avatar.outfit = self.outfitControl.selectedSegmentIndex;
-        self.avatar.mood = self.selectedIndexPath.row;
+        self.avatar.outfit = self.outfitSelectedIndexPath.row;
+        self.avatar.mood = self.moodSelectedIndexPath.row;
         [self.avatar save];
     }
 }
@@ -84,7 +108,11 @@
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return [self.avatarMoodImages count];
+    if (collectionView == self.moodCollectionView) {
+        return [self.avatarMoodImages count];
+    } else {
+        return [self.avatarOutfitImages count];
+    }
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
@@ -95,14 +123,19 @@
         cell = [[ImageCollectionViewCell alloc] init];
     }
     
-    cell.imageView.image = self.avatarMoodImages[indexPath.row];
+    if (collectionView == self.moodCollectionView) {
+        cell.imageView.image = self.avatarMoodImages[indexPath.row];
+    } else {
+        cell.imageView.image = self.avatarOutfitImages[indexPath.row];
+    }
     
     return cell;
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    [self highlightCellAtIndexPath:indexPath];
+    [self highlightCellAtIndexPath:indexPath
+                    collectionView:collectionView];
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath
@@ -115,15 +148,22 @@
 #pragma mark - Privates
 
 - (void)highlightCellAtIndexPath:(NSIndexPath *)indexPath
+                  collectionView:(UICollectionView *)collectionView
 {
-    UICollectionViewCell *cell = [self.moodCollectionView cellForItemAtIndexPath:indexPath];
+    UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
     [self addBorderToView:cell];
     
-    if (self.selectedIndexPath) {
-        [self.moodCollectionView deselectItemAtIndexPath:self.selectedIndexPath animated:YES];
+    if (collectionView == self.moodCollectionView) {
+        if (self.moodSelectedIndexPath) {
+            [collectionView deselectItemAtIndexPath:self.moodSelectedIndexPath animated:YES];
+        }
+        self.moodSelectedIndexPath = indexPath;
+    } else {
+        if (self.outfitSelectedIndexPath) {
+            [collectionView deselectItemAtIndexPath:self.outfitSelectedIndexPath animated:YES];
+        }
+        self.outfitSelectedIndexPath = indexPath;
     }
-    
-    self.selectedIndexPath = indexPath;
 }
 
 - (void)addBorderToView:(UIView *)view
